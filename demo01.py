3ceeee0deb0c229e852684e588304ad57b332988
zhang <1784452772@qq.com>
2020-08-04 10:30:45 +0800

    第三次提交

--- demo01.py
+++ demo01.py
@@ -4,7 +4,39 @@
 name=input("请输入你的姓名：")
 print("你的名字是：",name)  
 
-"""
+
 a=input("请输入a:")
 b=input("请输入b:")
 print("a和b的长度之和是：",len(a)+len(b))
+"""
+
+# t=(2,3,4,"hah","hahha","2",True,False)
+#print(t)
+#print(t[4])
+# print(t.index("hah"))
+# print(t.index(2))
+# print(t.count(3))
+
+# b=(t,"hahha","ttt")
+# print(b[0][3])   #二维元组
+
+s=[1,2,24,5,6]
+# s.append('haha')
+# print(s)
+
+# s.insert(2,'zhangsan')
+# print(s)    #向指定的位置插入值
+
+# b=s.pop(2)   #删除、剪切
+# print('b=',b)
+# print(s)
+
+s.clear()
+print(s)
+
+s.append(1)
+print(s)
+x=["nihao","hahha"]
+s.extend(s+x)
+print(s)
+
